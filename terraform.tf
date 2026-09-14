880e604b0716b9cd1b0b93f9777dc92486746764
Rawail Kamran <rawail.kamran1@gmail.com>
2026-09-14 21:51:49 +0400

    Deploy remotely

--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pragra-tfstate-file-2026"
-    key    = "env/dev/terraform-resources.tfstate"
+    key    = "env/dev/terraform-customvpc.tfstate"
     region = "ca-central-1"
   }
 }
